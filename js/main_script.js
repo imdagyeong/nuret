dd9c0a3343012a7d638f4345bff0d1c016decabd
imdagyeong <162244431+imdagyeong@users.noreply.github.com>
2024-03-12 10:04:32 +0900

    Add files via upload

---
+++ js/main_script.js
@@ -0,0 +1,17 @@
+setInterval(function(){
+    $("#main_slide ul").delay(2000);
+    $("#main_slide ul").animate({
+        marginLeft: '-1920px'
+    });
+    $("#main_slide ul").delay(2000);
+    $("#main_slide ul").animate({
+        marginLeft: '-3840px'
+    });
+    $("#main_slide ul").delay(2000);
+    $("#main_slide ul").animate({
+        marginLeft: '-5760px'
+    });
+    $("#main_slide ul").animate({
+        marginLeft: '0px'
+    },0);
+});
